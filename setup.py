e529b3041b3e4a6d049f94259ae5e1d0dc7b4890
Boris Raicheff <boris@MacBook-Pro.local>
2017-07-02 16:49:25 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+#
+# WTForms-Parsley
+#
+# Copyright (C) 2017 Boris Raicheff
+# All rights reserved
+#
+
+
+from setuptools import find_packages, setup
+
+
+setup(
+    name='WTForms-Parsley',
+    version='0.1.0',
+    description='WTForms-Parsley',
+    author='Boris Raicheff',
+    author_email='b@raicheff.com',
+    url='https://github.com/raicheff/wtforms-parsley',
+    packages=find_packages(),
+)
+
+
+# EOF
